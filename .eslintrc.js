c79e588259c0ed27fc5613d4f5c869cadd2ce6c4
Thomas Alberola <thomas.alberola@jellysmack.com>
2022-04-19 17:19:26 +0200

    Update storybook builder vite to last

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,36 +4,28 @@ module.exports = {
     ecmaVersion: 2021,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'eslint:recommended',
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
-  ],
+  extends: ['eslint:recommended', 'airbnb', 'airbnb/hooks', 'plugin:react/recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   plugins: ['simple-import-sort', 'prettier'],
   rules: {
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'prettier/prettier': ['error', {}, {
+      usePrettierrc: true
+    }],
     'jsx-a11y/accessible-emoji': 'off',
     'react/jsx-props-no-spreading': 'off',
     'import/extensions': 'off',
     'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton'],
-      },
-    ],
-  },
+    'jsx-a11y/anchor-is-valid': ['error', {
+      components: ['Link'],
+      specialLink: ['hrefLeft', 'hrefRight'],
+      aspects: ['invalidHref', 'preferButton']
+    }]
+  }
 };
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,27 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-controls',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-controls'],
   staticDirs: ['../src/assets'],
   core: {
-    builder: 'storybook-builder-vite',
+    builder: '@storybook/builder-vite'
   },
+
   async viteFinal(config) {
-    return {
-      ...config,
-      esbuild: {
-        ...config.esbuild,
+    return { ...config,
+      esbuild: { ...config.esbuild
       },
-      rollupOptions: {
-        ...config.rollupOptions,
+      rollupOptions: { ...config.rollupOptions,
         // Externalize deps that shouldn't be bundled
         external: ['react', 'react-dom'],
         output: {
           // Global vars to use in UMD build for externalized deps
           globals: {
             react: 'React',
-            'react-dom': 'ReactDOM',
-          },
-        },
-      },
+            'react-dom': 'ReactDOM'
+          }
+        }
+      }
     };
-  },
+  }
+
 };
